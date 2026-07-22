08dc4ecfb96ff13931dc55c59f4a65374cb48c7a
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-07-22 18:31:52 -0400

    Bug fixes

--- player/player.gd
+++ player/player.gd
@@ -10,8 +10,8 @@ const CAM_TILT_SPEED: float = 10.0
 # Only restores when the player hits the floor, not wall.
 var wall_jumps_left: int = 0
 var air_jumps_left: int = 0
-var jump_force: float = 7.5
-var wall_jump_force: float = 20.0
+var jump_force: float = 10.0
+var wall_jump_force: float = 15.0
 var gravity_scale: float = 2.0
 var wallrun_gravity_scale: float = 0.1
 var traction: float = 16.0 # Not used yet.
@@ -19,10 +19,9 @@ var air_traction: float = 2.0
 var speed: float = 6.0
 var wallrun_speed: float = 9.0
 var mouse_sensitivity: float = 0.004 # Should probably add a setting menu for this eventually.
-var was_on_wall := false
+var just_hit_wall := false
 
 @onready var head: Marker3D = $Head
-@onready var wall_detector: Area3D = $WallDetector
 
 
 func _ready() -> void:
@@ -30,50 +29,50 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var on_wall: bool = wall_detector.get_overlapping_bodies().size() > 0
-
 	var input: Vector2 = Input.get_vector(&"left", &"right", &"forward", &"backward")
 	var y: float = velocity.y
 	var current_speed: float = speed if is_on_floor() else wallrun_speed
-	var current_traction: float = traction if is_on_floor() or on_wall else air_traction
+	var current_traction: float = traction if is_on_floor() or is_on_wall() else air_traction
 
 	var target_vel: Vector3 = Utils.vec2_to_3(input * current_speed).rotated(Vector3.UP, rotation.y)
 	velocity = velocity.lerp(target_vel, current_traction * delta)
 	velocity.y = y
 
 	# NOTE: It's important that this is run before the if statements below.
-	if (Input.is_action_just_pressed(&"jump") and air_jumps_left > 0
-			and (is_on_floor() or not on_wall)):
+	if Input.is_action_just_pressed(&"jump") and air_jumps_left > 0 and not is_on_wall():
 		velocity.y = jump_force
 		air_jumps_left -= 1
 
 	if is_on_floor():
 		wall_jumps_left = MAX_WALL_JUMPS
 		air_jumps_left = MAX_AIR_JUMPS
+		just_hit_wall = false
+	#else:
+	if is_on_wall():
+		if not just_hit_wall:
+			velocity.y = maxf(0.0, velocity.y) # Stop falling when you hit a wall.
+			just_hit_wall = true
+
+		if Input.is_action_just_pressed(&"jump") and wall_jumps_left > 0:
+			const WALL_PUSHOFF_WEIGHT: float = 0.5
+			velocity = Vector3.UP.slerp(get_wall_normal(), WALL_PUSHOFF_WEIGHT
+					) * wall_jump_force
+			wall_jumps_left -= 1
 	else:
-		var current_gravity_scale: float = gravity_scale
-		if on_wall:
-			if not was_on_wall:
-				velocity.y = 0.0 # Stop gravity when you hit a wall.
-			current_gravity_scale *= wallrun_gravity_scale
+		just_hit_wall = false
 
-			if Input.is_action_just_pressed(&"jump") and wall_jumps_left > 0:
-				const WALL_PUSHOFF_WEIGHT: float = 0.5
-				velocity = Vector3.UP.slerp(get_wall_normal(), WALL_PUSHOFF_WEIGHT
-						) * wall_jump_force
-				wall_jumps_left -= 1
-
-		velocity += get_gravity() * current_gravity_scale * delta
+	var current_gravity_scale: float = gravity_scale
+	if just_hit_wall:
+		current_gravity_scale *= wallrun_gravity_scale
+	velocity += get_gravity() * current_gravity_scale * delta
 
 	var target_tilt: float = 0.0
-	# TODO: Make a way to get the normal if it's just touching wall_detector, and not the actual wall.
 	if is_on_wall():
 		target_tilt = -get_wall_normal().dot(global_basis.x) * WALL_CAM_TILT
 	# Might could switch to tweens later.
 	head.rotation.z = lerpf(head.rotation.z, target_tilt, CAM_TILT_SPEED * delta)
 
 	move_and_slide()
-	was_on_wall = on_wall
 
 
 func _input(event: InputEvent) -> void:
