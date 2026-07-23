8494ee28b75ac126c63b674c575f51087b084fa6
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-07-22 22:35:38 -0400

    Countdown prototype

--- hud/contract/contract.gd
+++ hud/contract/contract.gd
@@ -1,1 +1,15 @@
-class_name Contract extends VBoxContainer
+class_name Contract extends Label
+
+
+var init_counters: Dictionary[String, int]
+
+
+func _on_player_counters_changed(counters: Dictionary[String, int]) -> void:
+	text = ""
+	for counter: String in counters:
+		text += "%s: %s/%s\n" % [counter, counters[counter], init_counters[counter]]
+
+
+func _on_player_counters_initialized(counters: Dictionary[String, int]) -> void:
+	init_counters = counters
+	_on_player_counters_changed(counters)
--- player/player.gd
+++ player/player.gd
@@ -1,19 +1,30 @@
 class_name Player extends CharacterBody3D
 
 
+signal counters_changed(counters: Dictionary[String, int])
+signal counters_initialized(counters: Dictionary[String, int])
+
 const MAX_TILT: float = deg_to_rad(90.0)
 const MAX_WALL_JUMPS: int = 3
 const MAX_AIR_JUMPS: int = 1
 const WALL_CAM_TILT := deg_to_rad(15.0)
 const CAM_TILT_SPEED: float = 10.0
 
+var counters: Dictionary[String, int] = {
+	"health": 3,
+	"ammo": 5,
+	"jumps": 10,
+	"wall_jumps": 6,
+	"grappling_hooks": 3,
+	"time": 30,
+}
 # Only restores when the player hits the floor, not wall.
 var wall_jumps_left: int = 0
 var air_jumps_left: int = 0
 var jump_force: float = 10.0
-var wall_jump_force: float = 15.0
+var wall_jump_force: float = 20.0
 var gravity_scale: float = 2.0
-var wallrun_gravity_scale: float = 0.1
+var wallrun_gravity_scale: float = 0.05
 var traction: float = 16.0 # Not used yet.
 var air_traction: float = 2.0
 var speed: float = 6.0
@@ -30,6 +41,7 @@ var grapple_point := Vector3.INF # INF means not grappling
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	counters_initialized.emit(counters.duplicate())
 
 
 func _physics_process(delta: float) -> void:
@@ -48,9 +60,11 @@ func _physics_process(delta: float) -> void:
 	# Kind of a hack solution, so might need cleaning later.
 	# But it makes it so that you won't get an extra mid-air jump if you fall off
 	# a platform without jumping.
-	if Input.is_action_just_pressed(&"jump") and air_jumps_left > 0 and not is_on_wall():
+	if (Input.is_action_just_pressed(&"jump") and air_jumps_left > 0 and counters["jumps"] > 0
+			and not is_on_wall()):
 		velocity.y = jump_force
 		air_jumps_left -= 1
+		tick_counter("jumps")
 
 	if is_on_floor():
 		wall_jumps_left = MAX_WALL_JUMPS
@@ -64,11 +78,13 @@ func _physics_process(delta: float) -> void:
 			velocity.y = 0.0 # Stop gravity when you hit a wall.
 			just_hit_wall = true
 
-		if Input.is_action_just_pressed(&"jump") and wall_jumps_left > 0:
-			const WALL_PUSHOFF_WEIGHT: float = 0.5
+		if (Input.is_action_just_pressed(&"jump") and wall_jumps_left > 0
+				and counters["wall_jumps"] > 0):
+			const WALL_PUSHOFF_WEIGHT: float = 0.6
 			velocity = Vector3.UP.slerp(get_wall_normal(), WALL_PUSHOFF_WEIGHT
 					) * wall_jump_force
 			wall_jumps_left -= 1
+			tick_counter("wall_jumps")
 	else:
 		just_hit_wall = false
 
@@ -94,9 +110,27 @@ func _input(event: InputEvent) -> void:
 		head.rotation.x = clampf(head.rotation.x, -MAX_TILT, MAX_TILT)
 		rotation.y -= mouse_vel.x
 
-	if event.is_action_pressed(&"grappling_hook") and cursor.is_colliding():
+	if (event.is_action_pressed(&"grappling_hook") and cursor.is_colliding()
+			and counters["grappling_hooks"] > 0):
 		grapple_point = (cursor.get_collision_point() if cursor.is_colliding()
 				else cursor.to_global(cursor.target_position))
+		tick_counter("grappling_hooks")
 	if event.is_action_released(&"grappling_hook"):
 		rope_origin.scale.z = 0.001
 		grapple_point = Vector3.INF
+
+
+func tick_counter(counter: String) -> void:
+	counters[counter] -= 1
+	counters_changed.emit(counters.duplicate())
+
+
+func _on_timer_tick_timeout() -> void:
+	tick_counter("time")
+	if counters["time"] <= 0:
+		# Copied from fall_zone.gd. Later we'll need to find a way to link them together.
+		print("U looze")
+		get_tree().paused = true
+		await get_tree().create_timer(1.5).timeout
+		get_tree().paused = false
+		get_tree().reload_current_scene()
