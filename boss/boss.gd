9c1c47e44f07e31c34e590b2572e6435608a542a
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-07-24 20:20:07 -0400

    Boss health bar

--- boss/boss.gd
+++ boss/boss.gd
@@ -1,6 +1,23 @@
 class_name Boss extends Area3D
 
 
+signal annoyed
+signal defeated
+
+var health: int = 5
+
+@onready var annoyance_bar: TextureProgressBar = $AnnoyanceBar
+
+
+func _ready() -> void:
+	annoyance_bar.max_value = health
+	annoyance_bar.value = health
+
+
 func _on_body_entered(_body: Node3D) -> void:
 	print("U won!")
-	get_tree().paused = true
+	health -= 1
+	annoyance_bar.value = health
+	annoyed.emit()
+	if health <= 0:
+		defeated.emit()
