c832be4e36ab6927b4531765d025bf686ca77da2
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-07-25 22:12:54 -0400

    Change air jump icon

--- hud/contract/counter_hud/counter_hud.gd
+++ hud/contract/counter_hud/counter_hud.gd
@@ -7,7 +7,7 @@ const TYPE_ICONS: Dictionary[String, Texture2D] = {
 	"grappling_hooks": preload("uid://capwe5i801dju"),
 	"health": preload("uid://g7n3eo2i6qim"),
 	#"jumps": preload("uid://cac4awpdhhnif"),
-	"air_jumps": preload("uid://cac4awpdhhnif"),
+	"air_jumps": preload("uid://6gnxtueb3hwa"),
 	"enemies": preload("uid://gqoq3ted3u7"),
 	"time": preload("uid://cu1ayho6x7j4w"),
 }
